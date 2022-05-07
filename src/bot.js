381d678fea929d4137db1c09de8435d75a329f7f
SazidAhmed <sazidahmed.official@gmail.com>
2022-05-08 04:51:51 +0600

    working on slash commands

--- src/bot.js
+++ src/bot.js
@@ -1,11 +1,23 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Collection } = require('discord.js');
+const fs = require('fs');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+
 require('dotenv').config();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+const functions = fs.readdirSync('./src/functions').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
+const commandFolders = fs.readdirSync('./src/commands');
+
+
+// client.on('ready', () => {
+//   console.log(`Logged in as ${client.user.tag}!`);
+// });
 
 (async ()=>{
+    for (file of functions){
+        require(`./functions/${file}`)(client);
+    }
     client.login(process.env.token);
 })();
