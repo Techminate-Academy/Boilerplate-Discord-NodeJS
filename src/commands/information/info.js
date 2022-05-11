bf79bba7a777219f40ad72e19aa1016f72c234b5
SazidAhmed <sazidahmed.official@gmail.com>
2022-05-11 21:53:29 +0600

    working on sub commands

--- src/commands/information/info.js
+++ src/commands/information/info.js
@@ -4,12 +4,24 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('info')
 		.setDescription('Info based on demand!')
-        .addSubmitcommand(subcommand=>
-        subcommand
-            .setName('info')
-            .setDescription('Info based on demand!')
-            .addSubmitcommand(subcommand=>
+        .addSubcommand(subcommand=>
+            subcommand
+                .setName('user')
+                .setDescription('Gets information of a user mentioned')
+                .addUserOption(option => option.setName('target').setDescription('The user mentioned')))
+        .addSubcommand(subcommand=>
+            subcommand
+                .setName('server')
+                .setDescription('info about the server')),
+                
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+        if(interection.option.getSub() === 'user'){
+            const user = interaction.option.getUser('target');
+            if(user){
+                await interaction.reply(`Username : ${user.username}\n ID: ${user.id}`);
+            }else{
+                await interaction.reply(`Username : ${interaction.user.username}\n Your ID: ${interaction.user.id}`);
+            }
+        }
 	},
 };
