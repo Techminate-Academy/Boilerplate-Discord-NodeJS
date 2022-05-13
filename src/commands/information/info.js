9ea1a3194975cd2879fb442a3f577efdb04a6f3c
SazidAhmed <sazidahmed.official@gmail.com>
2022-05-13 23:55:52 +0600

    working on embed

--- src/commands/information/info.js
+++ src/commands/information/info.js
@@ -2,24 +2,24 @@ const {
     SlashCommandBuilder
 } = require('@discordjs/builders');
 const {
-    MessageEmbed
+    MessageEmbed, MessageAttachment
 } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('info')
-        .setDescription('Get info about a user or a server!')
+        .setDescription('Returns info based on input')
         .addSubcommand(subcommand =>
             subcommand
             .setName('user')
             .setDescription('Info about a user')
-            .addUserOption(option => option.setName('target').setDescription('The user')))
+            .addUserOption(option => option.setName('target').setDescription('The mentioned user')))
         .addSubcommand(subcommand =>
             subcommand
             .setName('server')
             .setDescription('Info about the server')),
 
-    async execute(interaction) {
+    async execute(interaction, client) {
         if (interaction.options.getSubCommand() === 'user') {
             const user = interaction.options.getUser('target');
             if (user) {
@@ -27,6 +27,8 @@ module.exports = {
                     .setTitle(`${user.username}'s Information: `)
                     .setDescription('This is a description')
                     .setURL('http://sazidahmed.com')
+                    .EmbedAuthorData('Sazid Ahmed', client.user.displayAvatarURL(), 'http://sazidahmed.tk')
+                    .setThumbnail(client.user.displayAvatarURL())
             await interaction.reply(`Username : ${user.username}\n ID: ${user.id}`);
         } else {
             await interaction.reply(`Username : ${interaction.user.username}\n Your ID: ${interaction.user.id}`);
