4080c08086a94805311d707c941d3675408ecde8
SazidAhmed <sazidahmed.official@gmail.com>
2022-05-13 23:25:44 +0600

    working on embed

--- src/commands/information/info.js
+++ src/commands/information/info.js
@@ -1,31 +1,40 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const {
+    SlashCommandBuilder
+} = require('@discordjs/builders');
+const {
+    MessageEmbed
+} = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('info')
-		.setDescription('Info based on demand!')
-        .addSubcommand(subcommand=>
+    data: new SlashCommandBuilder()
+        .setName('info')
+        .setDescription('Get info about a user or a server!')
+        .addSubcommand(subcommand =>
             subcommand
-                .setName('user')
-                .setDescription('Gets information of a user mentioned')
-                .addUserOption(option => option.setName('target').setDescription('The user mentioned')))
-        .addSubcommand(subcommand=>
+            .setName('user')
+            .setDescription('Info about a user')
+            .addUserOption(option => option.setName('target').setDescription('The user')))
+        .addSubcommand(subcommand =>
             subcommand
-                .setName('server')
-                .setDescription('info about the server')),
-                
-	async execute(interaction) {
-        if(interection.option.getSub() === 'user'){
-            const user = interaction.option.getUser('target');
-            if(user){
-                await interaction.reply(`Username : ${user.username}\n ID: ${user.id}`);
-            }else{
-                await interaction.reply(`Username : ${interaction.user.username}\n Your ID: ${interaction.user.id}`);
-            }
-        }else if(interection.option.getSubcommand() === 'server'){
-            await interaction.reply(`server name : ${interection.guild.name}\n Total Members : ${interaction.guild.memberCount}`)
-        }else{
-            await interection.reply('no sub command was used.')
+            .setName('server')
+            .setDescription('Info about the server')),
+
+    async execute(interaction) {
+        if (interaction.options.getSubCommand() === 'user') {
+            const user = interaction.options.getUser('target');
+            if (user) {
+                const userEmbed = new MessageEmbed()
+                    .setTitle(`${user.username}'s Information: `)
+                    .setDescription('This is a description')
+                    .setURL('http://sazidahmed.com')
+            await interaction.reply(`Username : ${user.username}\n ID: ${user.id}`);
+        } else {
+            await interaction.reply(`Username : ${interaction.user.username}\n Your ID: ${interaction.user.id}`);
         }
-	},
+    } else if (interaction.options.getSubCommand() === 'server') {
+        await interaction.reply(`server name : ${interaction.guild.name}\n Total Members : ${interaction.guild.membercount}`)
+    } else {
+        await interaction.reply('no sub command was used.')
+    }
+},
 };
