8773560a8f8afe5872d10aa03ed6a454f2e62459
SazidAhmed <sazidahmed.official@gmail.com>
2022-05-15 05:42:25 +0600

    working on select menu

---
+++ src/commands/information/color.js
@@ -0,0 +1,46 @@
+const {
+	SlashCommandBuilder
+} = require('@discordjs/builders');
+const {
+	MessageActionRow,
+	MessageSelectMenu
+} = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('Color')
+		.setDescription('Ask your favorite color'),
+
+	async execute(interaction, client) {
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageSelectMenu()
+				.setCustomId('select-color')
+				.setPlaceholder('Nothing selected')
+				.setMinValues(1)
+				.setMaxValues(2)
+				.addOptions([{
+						label: 'Green',
+						description: 'Your favorite color is Green',
+						value: 'Green',
+					},
+					{
+						label: 'Red',
+						description: 'Your favorite color is Red',
+						value: 'Red',
+					},
+					{
+						label: 'Blue',
+						description: 'Your favorite color is Blue',
+						value: 'Blue',
+					},
+				]),
+			);
+
+		await interaction.reply({
+			content: 'What is your favorite color?',
+			components: [row]
+		});
+
+	},
+};
