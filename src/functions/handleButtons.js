d305fd62417cd1e1c0b50ac8221843f3bdb4de09
SazidAhmed <sazidahmed.official@gmail.com>
2022-05-17 23:27:25 +0600

    working on button handler

--- src/functions/handleButtons.js
+++ src/functions/handleButtons.js
@@ -4,37 +4,17 @@ const clientId = '972621733783797770';
 const guildId = '966475714855579708';
 
 module.exports = (client) => {
-    client.handleButtons = async (commandFolders, path) => {
-        client.commandArray = [];
-        for (folder of commandFolders) {
-            const commandFiles = fs.readdirSync(`${path}/${folder}`).filter(file => file.endsWith('.js'));
-            for (const file of commandFiles) {
-                const command = require(`../commands/${folder}/${file}`);
+    client.handleButtons = async () => {
+        const buttonFolder =fs.readdirSync('src/buttons');
+        for (const folder of buttonFolders) {
+            const buttonFiles = fs.readdirSync(`./src/buttons/${folder}`).filter(file => file.endsWith('.js'));
+            for (const file of buttonFiles) {
+                const button = require(`../butons/${folder}/${file}`);
                 // Set a new item in the Collection
                 // With the key as the command name and the value as the exported module
                 client.commands.set(command.data.name, command);
                 client.commandArray.push(command.data.toJSON());
             }
         }
-
-        const rest = new REST({
-            version: '9'
-        }).setToken(process.env.token);
-
-        (async () => {
-            try {
-                console.log('Started refreshing application (/) commands.');
-
-                await rest.put(
-                    Routes.applicationGuildCommands(clientId, guildId), {
-                        body: client.commandArray
-                    },
-                );
-
-                console.log('Successfully reloaded application (/) commands.');
-            } catch (error) {
-                console.error(error);
-            }
-        })();
     }
 }
