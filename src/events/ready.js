dd740d9d6c2bc7a130bfb412af6caa618e7fabcc
SazidAhmed <sazidahmed.official@gmail.com>
2022-05-18 23:01:28 +0600

    working on presence status

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,5 +3,7 @@ module.exports = {
 	once: true,
 	async execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+
+		client.user.setPresence({activites:[{name: 'with disckod.js', type: 'player'}], status:'online'})
 	},
 };
